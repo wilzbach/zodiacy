d303bbecc837e554a59d8d2dea88ae82d74ddf79
Sebastian Wilzbach <seb@wilzba.ch>
2015-10-13 15:11:31 +0300

    adapted urls

--- generate_dates.py
+++ generate_dates.py
@@ -5,7 +5,7 @@ from datetime import date, datetime, timedelta
 from os.path import join
 from os import makedirs
 
-startDate = date(2007, 08, 01)
+startDate = date(2007, 8, 1)
 endDate = datetime.now()
 endDate = date(endDate.year, endDate.month, endDate.day)
 zodiaSigns = [
@@ -37,6 +37,6 @@ def perdelta(start, end, delta):
 for sign in zodiaSigns:
     with open(join(downloadFolder, sign), "w") as out:
         for res in perdelta(startDate, endDate, timedelta(days=1)):
-            res = "%s%s%s" % (res.year, res.month, res.day)
+            res = res.strftime("%Y-%m-%d")
             outHoro = "http://www.tarot.com/daily-horoscope/%s/%s" % (sign, res)
             print(outHoro, file=out)
