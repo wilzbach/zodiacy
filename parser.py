207e2763bb23ec0c163ccb6c775730771b82d94d
Max Schettler <max.schettler@posteo.de>
2015-10-13 17:44:54 +0300

    Remove parse code

--- parser.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def parse_horoscope(file):
-    pattern = '<div class="daily overview">\s+<h3 class="title">Daily Overview</h3>\s+<div class="desc">(.*) <a'
-    regex = re.compile(pattern)
-    text = ''.join(file.readlines())
-    result = regex.search(text)
-    return result.group(1) if result is not None else None
