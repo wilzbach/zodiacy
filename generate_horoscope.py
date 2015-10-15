432adc779086c2a7793b90141fddaec6b11ec494
Sebastian Wilzbach <seb@wilzba.ch>
2015-10-15 23:12:13 +0300

    use hmm and past emissions
    Former-commit-id: 035551aa17fef116bef7af935a0a04b0d2dc0bd1

--- generate_horoscope.py
+++ generate_horoscope.py
@@ -15,6 +15,13 @@ __copyright__ = "Copyright 2015, Project Zodiacy"
 _WORDNICK_API_URL = 'http://api.wordnik.com/v4'
 _WORDNICK_API_KEY = '4ee1d234e4faae42a13680b776b0e348960adc62f6f7238ed'
 
+
+def restricted_weight(x):
+    x = float(x)
+    if x < 0.0 or x > 100.0:
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 100.0]" % (x,))
+    return x
+
 _parser = argparse.ArgumentParser(description="Awesome horoscope generator")
 _parser.add_argument('-d', '--debug', dest='debug',
                      help='show debug logs', action='store_true')
@@ -30,8 +37,16 @@ _parser.add_argument('-o', '--order', dest='order',
                      help='order of the used markov chain', type=int, default=4)
 _parser.add_argument('-c', '--synonym', dest='use_synonym',
                      help='use also synonyms of keywords for generation', action='store_true')
-_parser.add_argument('-m', '--markov_type', dest='markov_type', choices=('markov', 'hmm'),
-                     help='Markov type to use (default: markov)', default="markov", action='store')
+_parser.add_argument('-m', '--markov_type', dest='markov_type', choices=('markov', 'hmm', 'hmm_past'),
+                     help='Markov type to use (default: markov)', default="markov")
+
+_parser.add_argument('--prob_hmm_states', dest='prob_hmm_states', type=restricted_weight,
+                     help='When using previous states and emissions, weight for the previous states',
+                     default=0.5)
+
+_parser.add_argument('--prob_hmm_emissions', dest='prob_hmm_emissions', type=restricted_weight,
+                     help='When using previous states and emissions, weight for the previous emissions',
+                     default=0.5)
 
 
 def config_logging(level):
@@ -54,5 +69,7 @@ if __name__ == '__main__':
                         with_synonyms=args.use_synonym, keyword=args.keyword,
                         wordnik_api_url=_WORDNICK_API_URL, wordnik_api_key=_WORDNICK_API_KEY)
     mk = Markov(corpus, order=args.order,
-                use_emissions=args.markov_type == "hmm")
+                use_emissions=args.markov_type[0:3] == "hmm",
+                prob_hmm_states=args.prob_hmm_states,
+                prob_hmm_emissions=args.prob_hmm_emissions)
     print(mk.generate_text(args.markov_type))
