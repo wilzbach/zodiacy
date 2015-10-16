1b24fcb67de5744ae12b34cc843f3151a7c2d59f
Sebastian Wilzbach <seb@wilzba.ch>
2015-10-16 07:56:06 +0300

    heroku with bottle
    Former-commit-id: 44a0dadfffbb309d785f12af72bda76dca2791df

--- srv/hello.py
+++ srv/hello.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
+import sqlite3
+from os import path
 from bottle import route, run
+from zodiacy.corpus import Corpus
+from zodiacy.markov import Markov
+
+here = path.abspath(path.dirname(__file__))
 
 
 @route("/")
 def hello_world():
-        return "Hello World!"
+        db = path.join(here, '..', 'data', 'zodiac.sqlite')
+        with sqlite3.connect(db) as conn:
+            corpus = Corpus(conn, keyword='pride')
+
+        mk = Markov(corpus)
+        return mk.generate_text('markov')
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
